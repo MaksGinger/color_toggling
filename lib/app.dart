f6dd57f928263cf48c2de3842dd10fbaa2ad687d
Maksimilian <maksimilianyak@gmail.com>
2021-09-11 14:34:18 +0400

    incapsulate AppColumn

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,5 @@
-import 'package:color_transformation/widgets/toggle_color_buttons.dart';
+import 'package:color_transformation/widgets/app_column.dart';
 import 'package:flutter/material.dart';
-import 'package:color_transformation/widgets/color_transform_box.dart';
 import 'package:color_transformation/theme/app_colors.dart';
 
 class ColorTransformApp extends StatelessWidget {
@@ -31,44 +30,6 @@ class ColorTransformWidget extends StatelessWidget {
       body: const Center(
         child: AppColumn(),
       ),
-    );
-  }
-}
-
-class AppColumn extends StatefulWidget {
-  const AppColumn({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  State<AppColumn> createState() => _AppColumnState();
-}
-
-class _AppColumnState extends State<AppColumn> {
-  Color boxColor = Colors.red;
-
-  void changeColor(Color newColor) {
-    setState(() {
-      boxColor = newColor;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: <Widget>[
-        ColorTransformBox(
-          width: 200,
-          height: 200,
-          backgroundColor: boxColor,
-          borderColor: Colors.black,
-        ),
-        ToggleColorButtons(
-          changeColorCallback: changeColor,
-        ),
-      ],
     );
   }
 }
